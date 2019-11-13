0d3d818ca5fee0887c6d9cbe9bf6b240db0c6941
Crawford <joelcrawford30@gmail.com>
2019-11-13 10:23:11 +0300

    Initial Commit

---
+++ ICHUZZ2WORK2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICHUZZ2WORK2
+//
+//  Created by JOEL CRAWFORD on 13/11/2019.
+//  Copyright © 2019 ICHUZZ2WORK2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
